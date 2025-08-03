9627e9b89f267f9dcb529ee0d0e90f00cf7c399b
Lucie Sedláková <luckasedlakova22@gmail.com>
2025-08-03 11:46:55 +0200

    change localstorage

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -2,11 +2,11 @@ import type { ShoppingList } from "../types/index";
 
 const STORAGE_KEY = "shoppingLists";
 
-export function loadShoppingLists(): ShoppingList[] {
+export const loadShoppingLists = (): ShoppingList[] => {
   const data = localStorage.getItem(STORAGE_KEY);
   return data ? JSON.parse(data) : [];
-}
+};
 
-export function saveShoppingLists(lists: ShoppingList[]) {
+export const saveShoppingLists = (lists: ShoppingList[]): void => {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(lists));
-}
+};
